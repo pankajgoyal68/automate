3cdcecf6024086d2a432236b52744527ba32fa09
pankajgoyal68 <55877621+pankajgoyal68@users.noreply.github.com>
2021-04-22 18:24:15 +0530

    reject commit file

---
+++ reject_commit.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+set -e
+
+zero_commit='0000000000000000000000000000000000000000'
+msg_regex='[CITY\-[0-9]+\]'
+
+while read -r oldrev newrev refname; do
+
+	# Branch or tag got deleted, ignore the push
+    [ "$newrev" = "$zero_commit" ] && continue
+
+    # Calculate range for new branch/updated branch
+    [ "$oldrev" = "$zero_commit" ] && range="$newrev" || range="$oldrev..$newrev"
+
+	for commit in $(git rev-list "$range" --not --all); do
+		if ! git log --max-count=1 --format=%B $commit | grep -iqE "$msg_regex"; then
+			echo "ERROR:"
+			echo "ERROR: Your push was rejected because the commit"
+			echo "ERROR: $commit in ${refname#refs/heads/}"
+			echo "ERROR: is missing the JIRA Issue 'CITY-123'."
+			echo "ERROR:"
+			echo "ERROR: Please fix the commit message and push again."
+			echo "ERROR: https://help.github.com/en/articles/changing-a-commit-message"
+			echo "ERROR"
+			exit 1
+		fi
+	done
+
+done
